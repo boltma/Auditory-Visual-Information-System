cf56cd3784573dbbfec5c6465e4306f2edcf7b6b
Xiaoyang Ma <boltma@gmail.com>
2020-11-04 15:43:34 +0800

    Update TS Model iteration

--- code/TSmodel.m
+++ code/TSmodel.m
@@ -1,10 +1,12 @@
-function I = TSmodel(b, s, as, nu, G)
+function [I, Im] = TSmodel(b, s, as, nu, G)
 %TSMODEL 此处显示有关此函数的摘要
 %   此处显示详细说明
     v = [0; 0; 1];
     Gb = G.' \ b;
     Gs = G * s;
     Gsnorm = Gs / norm(Gs);
-    I = b.' * s + (as .* norm(Gs) * exp(-nu.^2 .* (acos(Gb.' * (Gsnorm + v) / norm(Gb) / norm(Gsnorm + v))).^2)) ./ (Gb' * v / norm(Gb));
+    Id = b.' * s;
+    Im = (as .* norm(Gs) * exp(-nu.^2 .* (acos(Gb.' * (Gsnorm + v) / norm(Gb) / norm(Gsnorm + v))).^2)) ./ (Gb' * v / norm(Gb));
+    I = Id + Im;
 end
 
--- code/rendering.m
+++ code/rendering.m
@@ -15,44 +15,56 @@ t = angleConversion(Test);
 
 th_l = 5;
 th_h = 255;
+m_ori = m; % save original m before removing saturated or shadows
 m(m < th_l | m > th_h) = NaN;
 
-b = s'\m';
+as = 0;
+nu = 0;
+g = reshape(eye(3), 1, 9);
 
-% len = numel(b);
-% TSfunc = @(x) norm(TSmodel(reshape(x(1:len), 3, []), s, x(len+1), x(len+2), reshape(x(end-8:end), 3, 3)) - m);
-% x0 = [reshape(b, 1, []), 0, 0, reshape(eye(3), 1, 9)];
-% options = optimset('Display','iter','MaxIter', 1,'Algorithm','quasi-newton');
-% x = fminunc(TSfunc, x0, options);
+Im = zeros(size(m));
 
-k = zeros(168, 168);
-z = zeros(3, 168*168);
-for ind = 1:168*168
-    k(ind) = norm(b(:, ind));
-    z(:, ind) = b(:, ind) / k(ind);
-    z(:, ind) = -z(:, ind) / z(3, ind);
+for ii = 1:10
+    b = s'\(m - Im)';
+
+    k = zeros(168, 168);
+    z = zeros(3, 168*168);
+    for ind = 1:168*168
+        k(ind) = norm(b(:, ind));
+        z(:, ind) = b(:, ind) / k(ind);
+        z(:, ind) = -z(:, ind) / z(3, ind);
+    end
+
+    zx = reshape(z(1,:), 168, 168);
+    zy = reshape(z(2,:), 168, 168);
+    zx = Inpaint(zx);
+    zy = Inpaint(zy);
+    k = reshape(Inpaint(k), 1, []);
+
+    z = normalToDepth(zx, zy);
+
+    [zx_new, zy_new] = partial(z);
+    b_new = [-reshape(zx_new, 1, []); -reshape(zy_new, 1, []); -ones(1, 168*168)];
+    b_new = -b_new ./ vecnorm(b_new) .* k;
+    
+    TSfunc = @(x) norm((TSmodel(b_new, s, x(1), x(2), reshape(x(3:end), 3, 3)) - m_ori) .* ~isnan(m));
+    x0 = [0, 0, reshape(eye(3), 1, 9)];
+    options = optimset('Display', 'iter', 'Algorithm', 'quasi-newton');
+    x = fminunc(TSfunc, x0, options);
+
+    as = x(1);
+    nu = x(2);
+    g = reshape(x(3:end), 3, 3);
+    
+    for jj = 1:7
+        [~, Im(:, jj)] = TSmodel(b_new, s(:, jj), as, nu, g);
+    end
 end
 
-zx = reshape(z(1,:), 168, 168);
-zy = reshape(z(2,:), 168, 168);
-zx = Inpaint(zx);
-zy = Inpaint(zy);
-k = reshape(Inpaint(k), 1, []);
-
-depth = normalToDepth(zx, zy);
-z = depth;
-
-[zx_new, zy_new] = partial(depth);
-b_new = [reshape(zx_new, 1, []); reshape(zy_new, 1, []); -ones(1, 168*168)];
-b_new = -b_new ./ vecnorm(b_new) .* k;
-
-% b = reshape(x(1:len), 3, []);
-% as = x(len+1);
-% nu = x(len+2);
-% g = reshape(x(end-8:end), 3, 3);
 for ii = 1:10
-    imgs(:,:,ii) = uint8(reshape(b_new' * t(:, ii), 168, 168));
-    % imgs(:,:,ii) = uint8(reshape(TSmodel(b, t(:, ii), as, nu, g), 168, 168));
+    %imgs(:,:,ii) = uint8(reshape(b_new' * t(:, ii), 168, 168));
+    [I, ~] = TSmodel(b_new, t(:, ii), as, nu, g);
+    imgs(:,:,ii) = uint8(reshape(I, 168, 168));
 end
 
 end
